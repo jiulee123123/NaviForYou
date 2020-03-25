4b8476563591299f34703fe8a614ed4fc5f76b09
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-03-25 13:41:54 +0900

    JIU 0325
    click the button -> json file parse/fetch

--- app/src/main/java/com/example/naviforyou/MainActivity.java
+++ app/src/main/java/com/example/naviforyou/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.naviforyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button click;
+    public static TextView data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.button_start);
+        data = (TextView) findViewById(R.id.flecheddata);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });
     }
 }
